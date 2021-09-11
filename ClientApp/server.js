f4d0942de8ab68c8b5bb1cccd2e151cfb86594c7
Ma7iasBo7ana <matiasbotana1@gmail.com>
2021-09-11 19:33:55 -0300

    cambios en server.json

--- ClientApp/server.js
+++ ClientApp/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/angular-aplicacion-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/angular-aplicacion-heroku/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5001);
+app.listen(process.env.PORT || 8080);
